efeabd253685f3235e0577500d4690d028d3f6a5
stanislavnaydenov <stanislav.naydenov@fdiba.tu-sofia.bg>
2023-10-25 14:41:02 +0300

    Create test.py

---
+++ central/test.py
@@ -0,0 +1,14 @@
+# make the import later on
+
+
+# Central access token and credentials
+
+central_info = {
+    "base_url": "https://internal-apigw.central.arubanetworks.com/",
+    "token": {
+        "access_token": "jdp2jCXI0vZ7RJzi4RMwHLbO48qbSNsz"
+    }
+}
+
+ssl_verify = True
+
